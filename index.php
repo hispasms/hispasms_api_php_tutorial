8e91629412eb2c26cf8fd728dc895a0e421686f3
HispaSMS <dev@hispasms.com>
2017-01-01 20:08:34 +0100

    Added bootstrap. xml -> json. Refactoring.

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <title>HispaSMS API PHP Tutorial</title>
+</head>
+<body>
+<div class="container">
+    <div class="page-header">
+        <h1>Sent messages logs</h1>
+    </div>
+    <div>
+        <ul class="nav nav-pills nav-stacked">
+            <li><h4><a href="advancedSms.php">Fully featured textual message</a></h4></li>
+            <li><h4><a href="logs.php">Sent messages logs</a></h4></li>
+            <li><h4><a href="dlrPush.php">Delivery reports on Notify URL</a></h4></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
